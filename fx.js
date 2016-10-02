db8694aa989435515e85b0536b628f3e48e489d7
Omar <stellaar.oz@gmail.com>
2016-10-02 19:24:02 -0300

    improved feat buttons

--- fx.js
+++ fx.js
@@ -13,3 +13,23 @@ $(document).ready(function() {
   $('.feat').append('<span class="glyphicon glyphicon-collapse-down"; \
   id="mini-glyph"></span>');
 });
+
+// cambio de color con mouse
+
+$(document).ready(function() {
+  $('.feat').hover(function() {
+    $(this).css('background-color', 'lightcyan');
+  }, function() {
+    $(this).css('background-color', 'lightblue');
+  });
+});
+
+// aclaracion del color con mouse
+
+$(document).ready(function() {
+  $('.feat').hover(function() {
+    $(this).fadeTo('fast', 0.8);
+  }, function() {
+    $(this).fadeTo('fast', 1);
+  });
+});
